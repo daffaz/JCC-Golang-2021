71a524348750b2f886d0fc0fd13deee072ce0ac9
Daffa <daffazakyy8@gmail.com>
2021-09-10 00:47:18 +0700

    finish tugas 9

--- Tugas-9/tugas9.go
+++ Tugas-9/tugas9.go
@@ -11,27 +11,49 @@ func printJabarcoding(kalimat string, tahun int) {
 	fmt.Println(kalimat, tahun)
 }
 
-func kelilingSegitigaSamaSisi(sisi int, hitungKeliling bool) (string, error) {
+func kelilingSegitigaSamaSisi(sisi int, hitungKeliling bool) (str string, err error) {
 	if hitungKeliling == true {
 		if sisi > 0 {
-			return "keliling segitiga sama sisinya dengan sisi " + strconv.Itoa(sisi) + " cm adalah " + strconv.Itoa(sisi*3) + " cm", nil
+			str = "keliling segitiga sama sisinya dengan sisi " + strconv.Itoa(sisi) + " cm adalah " + strconv.Itoa(sisi*3) + " cm"
+			err = nil
 		} else {
-			return "", errors.New("Maaf anda belum menginput sisi dari segitiga sama sisi")
+			str = ""
+			err = errors.New("Maaf anda belum menginput sisi dari segitiga sama sisi")
 		}
 	} else {
 		if sisi > 0 {
-			return strconv.Itoa(sisi), nil
+			str = strconv.Itoa(sisi)
+			err = nil
 		} else {
-			defer recover()
-			panic("wow")
-			return "", errors.New("Maaf anda belum menginput sisi dari segitiga sama sisi")
+			defer func() {
+				if r := recover(); r != nil {
+					str = "Maaf anda belum menginput sisi dari segitiga sama sisi"
+					err = errors.New(r.(string))
+				}
+			}()
+			panic("PANIC!!")
 		}
 	}
+	return
+}
+
+// Soal 3
+
+func tambahAngka(angka int, varAngka *int) {
+	*varAngka += angka
+}
+
+func deferSoal3(totalAngka *int) {
+	fmt.Println(*totalAngka)
+	fmt.Println("Soal 3 selesai")
 }
 
 func main() {
+	angka := 1
+	defer deferSoal3(&angka)
 	// Soal 1
 	defer printJabarcoding("Candradimuka Jabar Coding Camp", 2021)
+	// End soal 1
 
 	// Soal 2
 	fmt.Println()
@@ -41,4 +63,15 @@ func main() {
 	fmt.Println(kelilingSegitigaSamaSisi(8, false))
 	fmt.Println(kelilingSegitigaSamaSisi(0, true))
 	fmt.Println(kelilingSegitigaSamaSisi(0, false))
+
+	// Soal 3
+	fmt.Println()
+	fmt.Println("-------------Soal 3")
+
+	// deklarasi variabel angka ini simpan di baris pertama func main
+	// defer deferSoal3(&angka)
+	tambahAngka(7, &angka)
+	tambahAngka(6, &angka)
+	tambahAngka(-1, &angka)
+	tambahAngka(9, &angka)
 }
